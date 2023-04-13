8384193caaeca3d87e223b92bb5b126c1a938435
30615 <129129756+nayoung1012@users.noreply.github.com>
2023-04-13 01:31:16 +0000

    if else에 대해 배웠다. 오늘은 이해가 잘되었다!

--- if_else.c
+++ if_else.c
@@ -1,17 +1,17 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// main() {
-//   int a = 5;
+main() {
+  int a = 5;
 
-//   if (a>3)    //if 참
-//     printf("A\n");   //조건문이 참이면 실행
-//   else
-//     printf("B\n");   //조건문이 거짓이면 실행
-//   printf("C\n");   //들여쓰기를 했다면 else에 해당돼서 출력 안됨(들여쓰기 중요)
+  if (a>3)    //if 참
+    printf("A\n");   //조건문이 참이면 실행
+  else
+    printf("B\n");   //조건문이 거짓이면 실행
+  printf("C\n");   //들여쓰기를 했다면 else에 해당돼서 출력 안됨(들여쓰기 중요)
 
-//   if (a>8) printf("D\n");   //조건문이 참이면 실행
+  if (a>8) printf("D\n");   //조건문이 참이면 실행
 
-//   else printf("E\n");   //조건문이 거짓이면 실행
+  else printf("E\n");   //조건문이 거짓이면 실행
 
-//   printf("F\n");   //들여쓰기를 했다면 else에 해당돼서 출력 안됨(들여쓰기 중요)
-// }
+  printf("F\n");   //들여쓰기를 했다면 else에 해당돼서 출력 안됨(들여쓰기 중요)
+}
